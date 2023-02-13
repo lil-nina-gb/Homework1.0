f3545a0033d47fdb3aa09501ca098b27572320ec
Антонина Акимова <antoninaakimova@MacBook-Air-Antonina.local>
2023-02-13 21:23:56 +0700

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number % 2 == 0)
+    Console.WriteLine("Число четное");
+else 
+Console.WriteLine("Число нечетное");
